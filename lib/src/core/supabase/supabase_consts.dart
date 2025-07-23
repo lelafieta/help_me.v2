679d82cc04e5f9e023e502b84c01a23096c51ae1
lelafieta <lelafieta11@gmail.com>
2025-07-23 15:28:13 +0100

    Adicionando projeto ao repositório existente

---
+++ lib/src/core/supabase/supabase_consts.dart
@@ -0,0 +1,29 @@
+class SupabaseConsts {
+  static const String blogs = 'blogs';
+  static const String campaignComments = 'campaign_comments';
+  static const String campaignContributors = 'campaign_contributors';
+  static const String campaignDocuments = 'campaign_documents';
+  static const String campaignMidias = 'campaign_midias';
+  static const String campaignUpdates = 'campaign_updates';
+  static const String campaigns = 'campaigns';
+  static const String campaignsWithContributors = 'campaigns_with_contributors';
+  static const String categories = 'categories';
+  static const String communities = 'communities';
+  static const String events = 'events';
+  static const String favorites = 'favorites';
+  static const String feeds = 'feeds';
+  static const String notifications = 'notifications';
+  static const String ongs = 'ongs';
+  static const String ongsDocuments = 'ongs_documents';
+  static const String payments = 'payments';
+  static const String profiles = 'profiles';
+  static const String updates = 'updates';
+
+  static const String userFcmTokens = 'user_fcm_tokens';
+}
+
+class SupabaseStorageConsts {
+  static String midias = 'midias';
+  static String documents = 'documents';
+  static String updates = 'updates';
+}
